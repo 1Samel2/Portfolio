faddd4ce6ac1189cf925ff62fd612bf05e2193a3
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-01-19 14:48:22 -0300

    Retirando imagens svg

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const hamburguer = document.querySelector(".hamburguer")
-
-hamburguer.addEventListener("click", function(){
+document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-})
+)
