36d5fa0d8a7a40f3c221e7477d24f2bd89fa22f9
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-01-21 12:16:46 -0300

    Desenvolvimento

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 )
 
-alert("Site em Desenvolvimento")
+alert("O projeto de portfólio está em desenvolvimento")
